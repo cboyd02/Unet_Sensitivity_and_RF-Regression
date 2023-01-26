f8cfddc9b3375d0a871247969da16dd3b26f9843
cboyd02 <64119574+cboyd02@users.noreply.github.com>
2023-01-26 15:28:40 +1030

    Delete IRCAD directory

--- IRCAD/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .custom_unet import custom_unet
-from .custom_vnet import custom_vnet
-from .vanilla_unet import vanilla_unet
-from .satellite_unet import satellite_unet
-from .basic_unet import basic_unet
